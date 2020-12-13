aa9f22e5a74fecc16d0e8e6dda0506fe0ba2a217
Muhammad Hamad <ihamadrajpoot@users.noreply.github.com>
2020-12-13 13:54:20 +0500

    Added BookController BookModel, Views, and Libman jQuery and Bootstrap

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository bookRepository;
+
+        public BookController()
+        {
+            bookRepository = new BookRepository();
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return bookRepository.GetAllBooks();
+        }
+
+        public Book GetBook(int id)
+        {
+            return bookRepository.GetBookById(id);
+        }
+
+        public List<Book> SearchBooks(string bookName, string authorName)
+        {
+            return bookRepository.SearchBook(bookName,authorName);
+        }
+    }
+}
---
+++ BookStore/BookStore/Repository/BookRepository.cs
@@ -0,0 +1,37 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<Book> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public Book GetBookById(int Id)
+        {
+            return DataSource().Where(x => x.ID == Id).FirstOrDefault();
+        }
+
+        public List<Book> SearchBook(string title, string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(authorName)).ToList();
+        }
+
+        private List<Book> DataSource()
+        {
+            return new List<Book>
+            {
+                new Book{ID = 1, Title = "React", Author= "Hamad"},
+                new Book{ID = 2, Title = "C#", Author= "Imran"},
+                new Book{ID = 3, Title = "Java", Author= "Usman"},
+                new Book{ID = 4, Title = "Python", Author= "Hamza"},
+            };
+        }
+    }
+}
